93682faccfef7e7aaa0dd6bbf85465fcae585270
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-03 12:26:50 -0600

    Ajustando flujo inicial

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import divMain from './src/htmlComponents/divMain.html?raw';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <div class="appdiv">
-    </div>
-  </div>
-`
+document.querySelector('#app').innerHTML = divMain;
 
