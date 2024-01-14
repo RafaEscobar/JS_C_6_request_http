7566808a412c21585175ec7a23b1dda4a63a17ce
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-13 19:27:33 -0600

    Api consumida y listada, faltan ajustes visuales

--- src/anime/animeComponent.js
+++ src/anime/animeComponent.js
@@ -10,28 +10,21 @@ const fetchData = async() => {
  * @param {HTMLDivElement} element
  */
 export const animeComponent = (element) => {
-
-    const animeCard = document.createElement("div");
-
     const generateCard = (data) => {
-
-        for (const element of data) {
+        for (const item of data) {
+            const animeCard = document.createElement("div");
             animeCard.innerHTML = `
-            <div class="p-4 bg-gray-800 w-56 text-white rounded-xl">
-                <img src="${element.images.jpg.image_url}" alt="" width="" class="shadow-white shadow-sm">
-                <span class="block font-semibold text-center mt-2">${element.title}</span>
-                <span class="block text-center text-blue-300 text-sm mt-2 hover:text-blue-200">
-                    <a href="${element.trailer.url}">Trailer</a>
-                </span>
-            </div>
+                <div class="p-4 bg-gray-800 w-56 text-white rounded-xl">
+                    <img src="${item.images.jpg.image_url}" alt="" width="" class="shadow-white shadow-sm">
+                    <span class="block font-semibold text-center mt-2">${item.title}</span>
+                    <span class="block text-center text-blue-300 text-sm mt-2 hover:text-blue-200">
+                        <a href="${item.trailer.url}">Trailer</a>
+                    </span>
+                </div>
             `;
             element.appendChild(animeCard);
         }
-
     }
-
     fetchData()
         .then( generateCard );
-
-
 }
